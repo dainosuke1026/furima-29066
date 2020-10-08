d3d548c84ec67448c08bc862c2c6bd083b96f319
dainosuke1026 <daiichirou929@gmail.com>
2020-10-08 23:01:42 +0900

    エラーハンドリング設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,23 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :password, confirmation: true
-  validates :password_confirmation, presence: true
+
+  validates :password, confirmation: true #パスワードと確認用パスワードが一致しているかのバリデーション
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+
+  validates :birth_day, presence: true
+
+  validates :last_name, presence: true
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" }
+  
+  validates :first_name, presence: true
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" }
+  
+  validates :last_name_kana, presence: true
+  validates :last_name_kana, format: { with: /\A[ァ-ン]+\z/, message: "Full-width katakana characters" }
+  
+  validates :first_name_kana, presence: true
+  validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/, message: "Full-width katakana characters" }
 end
