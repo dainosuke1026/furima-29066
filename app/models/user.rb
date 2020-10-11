faf34cc7f9051c6b558cb0efb1d600e1a2f01fba
dainosuke1026 <daiichirou929@gmail.com>
2020-10-11 18:34:38 +0900

    バリデーションの正規表現変数化

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  REAL_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  FRIGANA_REGEX = /\A[ァ-ン]+\z/
+
   with_options presence: true do
     validates :nickname
     validates :birth_day
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
-    validates :last_name_kana, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' }
-    validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' }
+    validates :last_name, format: { with: REAL_NAME_REGEX, message: 'Full-width characters' }
+    validates :first_name, format: { with: REAL_NAME_REGEX, message: 'Full-width characters' }
+    validates :last_name_kana, format: { with: FRIGANA_REGEX, message: 'Full-width katakana characters' }
+    validates :first_name_kana, format: { with: FRIGANA_REGEX, message: 'Full-width katakana characters' }
   end
 
   validates :password, confirmation: true # パスワードと確認用パスワードが一致しているかのバリデーション
