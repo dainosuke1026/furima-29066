7358870614a64acf2366726be30a426d004024d5
dainosuke1026 <daiichirou929@gmail.com>
2020-10-11 15:32:18 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
 
-  validates :password, confirmation: true #パスワードと確認用パスワードが一致しているかのバリデーション
+  validates :password, confirmation: true # パスワードと確認用パスワードが一致しているかのバリデーション
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
@@ -14,14 +14,14 @@ class User < ApplicationRecord
   validates :birth_day, presence: true
 
   validates :last_name, presence: true
-  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" }
-  
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
+
   validates :first_name, presence: true
-  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" }
-  
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
+
   validates :last_name_kana, presence: true
-  validates :last_name_kana, format: { with: /\A[ァ-ン]+\z/, message: "Full-width katakana characters" }
-  
+  validates :last_name_kana, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' }
+
   validates :first_name_kana, presence: true
-  validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/, message: "Full-width katakana characters" }
+  validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,18 +1,18 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.name}
-    email {Faker::Internet.free_email}
+    nickname { Faker::Name.name }
+    email { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
-    birth_day {Faker::Date.birthday}
+    password { password }
+    password_confirmation { password }
+    birth_day { Faker::Date.birthday }
 
     transient do
-      person {Gimei.name}
+      person { Gimei.name }
     end
-    last_name {person.last.kanji}
-    first_name {person.first.kanji}
-    last_name_kana {person.last.katakana}
-    first_name_kana {person.first.katakana}
+    last_name { person.last.kanji }
+    first_name { person.first.kanji }
+    last_name_kana { person.last.katakana }
+    first_name_kana { person.first.katakana }
   end
 end
