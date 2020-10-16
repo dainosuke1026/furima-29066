7e7add4cffc56345f94baf602f259998416cb3a7
dainosuke1026 <daiichirou929@gmail.com>
2020-10-16 18:31:17 +0900

    バリデーション正規表現変数化

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,11 +9,13 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :day
 
+  PRICE_REGEX = /\A[1-9]+\z/.freeze
+  
   with_options presence: true do
     validates :image
     validates :name
     validates :description
-    validates :price, format: { with: /\A[1-9]+\z/, message: "Half-width number" }
+    validates :price, format: { with: PRICE_REGEX, message: "Half-width number" }
   end
 
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
