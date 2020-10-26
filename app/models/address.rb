77b28ec2c3c8399cb079254bbcd5ce649a5724bd
dainosuke1026 <daiichirou929@gmail.com>
2020-10-26 21:02:23 +0900

    配送先情報のバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,18 @@ class Address < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+
+  POSTAL_CODE_REGEX = /\A\d{3}[-]\d{4}\z/.freeze
+  PHONE_NUMBER_REGEX = /\A[0-9]+\z/.freeze
+
+  with_options presence: true do
+    validates :postal_code, format: { with: POSTAL_CODE_REGEX, message: 'Input correctly'}
+    validates :city
+    validates :house_number
+    validates :phone_number, format: { with: PHONE_NUMBER_REGEX, message: 'Input only number' }
+  end
+
+  with_options numericality: { other_than: 1, message: 'Select' } do
+    validates :prefecture_id
+  end
 end
