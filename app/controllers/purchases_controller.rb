195eae4dec9d3cff626b2337bb3f590b70ee8d6e
dainosuke1026 <daiichirou929@gmail.com>
2020-10-30 15:00:26 +0900

    クレジット決済機能

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -10,23 +10,36 @@ class PurchasesController < ApplicationController
   def create
     @purchase_address = PurchaseAddress.new(address_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to controller: :items, action: :index
     else
       render 'new'
     end
   end
-
+  
   private
 
+  def item_data
+    Item.find(params[:item_id])
+  end
+  
   def move_to_index
-    @item = Item.find(params[:item_id])
-    if current_user.id == @item.user.id
+    if current_user.id == item_data.user_id
       redirect_to controller: :items, action: :index
     end
   end
 
   def address_params
-    params.require(:purchase_address).permit(:prefecture_id, :postal_code, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_address).permit(:prefecture_id, :postal_code, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token], price: item_data.price)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: address_params[:price],
+        card: address_params[:token],
+        currency: 'jpy'
+      )
   end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :prefecture_id, :postal_code, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :prefecture_id, :postal_code, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token, :price
 
   POSTAL_CODE_REGEX = /\A\d{3}[-]\d{4}\z/.freeze
   PHONE_NUMBER_REGEX = /\A[0-9]+\z/.freeze
@@ -10,6 +10,7 @@ class PurchaseAddress
     validates :city
     validates :house_number
     validates :phone_number, format: { with: PHONE_NUMBER_REGEX, message: 'Input only number' }
+    validates :token, presence: true
   end
 
   with_options numericality: { other_than: 1, message: 'Select' } do
